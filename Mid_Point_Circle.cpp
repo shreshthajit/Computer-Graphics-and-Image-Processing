21ae0df56df13c6d6a3705822b32174ef32ea36d
Shreshthajit Das <43321488+shreshthajit@users.noreply.github.com>
2023-01-17 01:17:01 +0600

    Update Mid_Point_Circle.cpp

--- Mid_Point_Circle.cpp
+++ Mid_Point_Circle.cpp
@@ -1,94 +1,75 @@
-#include<cstdlib>
-#include<cstdio>
-#ifdef __APPLE__
-#include <GLUT/glut.h>
-#else
+ #include <iostream>
 #include <GL/glut.h>
-#endif
-#include<cmath>
+using namespace std;
+int r, xc, yc;
 
-
-float x=0,y,x2,y2,m,i,j,p;
-int dx=0,dy=0,r;
-void display(void)
+void plot(int x, int y)
 {
-    /* clear all pixels */
-    glClear (GL_COLOR_BUFFER_BIT);
-    /* draw white polygon (rectangle) with corners at
-    * (0.25, 0.25, 0.0) and (0.75, 0.75, 0.0)
-    */
-    glEnd();
+  glBegin(GL_POINTS);
+  glVertex2i(x + xc, y + yc);
+  glEnd();
+}
 
-    glColor3f (0.0, 1.0, 0.0);
-    glBegin(GL_POINTS);
-    p=1-r;
-    while((x<=y))
+void myInit(void)
+{
+    glClearColor(1.0, 1.0, 1.0, 0.0);
+	glColor3f(0.0f, 0.0f, 0.0f);
+	glPointSize(4.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0.0, 640.0, 0.0, 480.0);
+}
+
+void midPointCircleAlgo()
+{
+  int x = 0;
+  int y = r;
+  float decision = 5 / 4 - r;
+  plot(x, y);
+  while (y > x)
+  {
+    if (decision < 0)
     {
-        if(p<0)
-        {
-            x=x+1;
-            y=y;
-            printf("%0.2f %0.2f\n",x,y);
-            p=p+(2*x)+1;
-        }
-
-
-        else
-        {
-            x=x+1;
-            y=y-1;
-            printf("%0.2f %0.2f\n",x,y);
-            p=p+(2*x)+1-(2*y);
-        }
-        glVertex3f (((x/100)), ((y/100)), 0.0);
-        glVertex3f (((y/100)), ((x/100)), 0.0);
-        glVertex3f ((-(x/100)), (-(y/100)), 0.0);
-        glVertex3f ((-(x/100)), ((y/100)), 0.0);
-        glVertex3f (((x/100)), (-(y/100)), 0.0);
-        glVertex3f (((y/100)), (-(x/100)), 0.0);
-        glVertex3f ((-(y/100)), (-(x/100)), 0.0);
-        glVertex3f ((-(y/100)), ((x/100)), 0.0);
+        x++;
+        decision += 2 * x + 1;
     }
-    glEnd();
-
-    /* don't wait!
-    * start processing buffered OpenGL routines
-    */
-    glFlush ();
+    else
+    {
+        y--;
+        x++;
+        decision += 2 * (x - y) + 1;
+    }
+      plot(x, y);
+      plot(x, -y);
+      plot(-x, y);
+      plot(-x, -y);
+      plot(y, x);
+      plot(-y, x);
+      plot(y, -x);
+      plot(-y, -x);
+    }
 }
-void init (void)
+
+void myDisplay(void)
 {
-    /* select clearing (background) color */
-    glClearColor (0.0, 0.0, 0.0, 0.0);
-    /* initialize viewing values */
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
-    /**
-    gluOrtho2D(-300, 300, 0, 600);
-
-    */
+  glClear(GL_COLOR_BUFFER_BIT);
+  glColor3f(0.0, 0.0, 0.0);
+  glPointSize(1.0);
+  midPointCircleAlgo();
+  glFlush();
 }
-/*
-* Declare initial window size, position, and display mode
-* (single buffer and RGBA). Open window with "hello"
-* in its title bar. Call initialization routines.
-* Register callback function to display graphics.
-* Enter main loop and process events.
-*/
+
 int main(int argc, char** argv)
 {
+    printf("Enter centre and radius of circle\n");
+    scanf("%d %d %d",&xc, &yc, &r);
 
-    printf("Enter radius: ");
-    scanf("%d",&r);
-    y=r;
     glutInit(&argc, argv);
-    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize (500, 500);
-    glutInitWindowPosition (100, 100);
-    glutCreateWindow ("hello");
-    init ();
-    glutDisplayFunc(display);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(640, 480);
+    glutInitWindowPosition(100, 150);
+    glutCreateWindow("Line Drawing Alogrithms");
+    glutDisplayFunc(myDisplay);
+    myInit();
     glutMainLoop();
-    return 0; /* ISO C requires main to return int. */
 }
